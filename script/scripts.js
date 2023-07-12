4fc51d3bdacad8178c6341c8b5823d663e4afc96
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2023-07-12 17:33:14 -0300

    Light mode include

--- script/scripts.js
+++ script/scripts.js
@@ -1,6 +1,29 @@
 const changeThemeBtn = document.querySelector("#mudar-tema")
 
-changeThemeBtn.addEventListener("change", function() {
-
+// Toggle light mode
+function toggleLigthMode()
+{
     document.body.classList.toggle("light");
-})
+}
+
+// Load Light ou Dark mode
+function loadTema(){
+    const lightmode = localStorage.getItem("light");
+
+    if(lightmode){
+        toggleLigthMode();
+    }
+}
+
+loadTema();
+
+changeThemeBtn.addEventListener("change", function() {
+    toggleLigthMode();
+
+    localStorage.removeItem("light");
+   
+    // Salvar ou remover ligth mode
+    if(document.body.classList.contains("light")){
+        localStorage.setItem("light", 1);
+    }
+});
